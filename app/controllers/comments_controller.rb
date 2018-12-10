c4cdbbe2d4b46df60c6845f3f3cbe20c72212e3c
Chris Topher <programmingfire@gmail.com>
2018-12-10 11:31:21 -0600

    Final Commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,10 @@ class CommentsController < ApplicationController
     end
 
     def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to post_path(@post)
     end
 
     def post_params
