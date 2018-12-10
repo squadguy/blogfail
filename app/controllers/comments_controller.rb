4480a9bb72b36e6f2a9539dd4ed4d8baa8b2e3a5
Chris Topher <programmingfire@gmail.com>
2018-12-10 11:06:31 -0600

    Fix Leave comment bug

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,18 @@
 class CommentsController < ApplicationController
 
     def create
-        @post = Post.find(params[]:post_id])
-        @comment = @post.commments.create(params[:comment]).permit(:name, :comment)
+        @post = Post.find(params[:post_id])
+        #Below doesn't work.  Working Code below it.
+        #@comment = @post.comments.create(params[:comment]).permit(:name, :comment)
+        @comment = @post.comments.create(post_params)
 
         redirect_to post_path(@post)
     end
 
     def destroy
+    end
+
+    def post_params
+        params.require(:comment).permit(:name, :comment)
     end
 end
