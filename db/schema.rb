3d2a2daf3be85e1e7f63651e9aff035a59db3f61
Chris Topher <programmingfire@gmail.com>
2018-12-09 19:45:42 -0600

    Fix Comment and Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_09_232159) do
+ActiveRecord::Schema.define(version: 2018_12_10_013313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
